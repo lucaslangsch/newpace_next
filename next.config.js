c781c7d7cd3a906d6aa147ad5e7477a63cbbb3cb
lucaslangsch <lucas.langsch@gmail.com>
2024-02-07 08:19:31 -0300

    fix key

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    pKey: 'TEST-fd35b829-6906-4d8c-a5af-e9d26976c69e',
-    auth: 'TEST-5478545679359546-071217-c2defb4b6ce26c85472e5c9928ee6557-204596992',
-    differencialPrincing: '204596992'
+    pKey: 'APP_USR-0b163835-1ede-4bee-a668-4348505a06a8',
+    auth: 'APP_USR-3988919391260874-020707-76c336c07245136756da3a1e56854a06-191362687',
+    differencialPrincing: '3988919391260874'
   }
 }
 
